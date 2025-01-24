c6b47bdb1306737e07172f49e5d24aa9d4cd0516
skippy <66940824+skippybossx@users.noreply.github.com>
2025-01-24 18:00:07 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Function to find the first available CTID
+# Funkcja do znalezienia pierwszego wolnego CTID
 get_next_ctid() {
     local ctid=100
     while pct status $ctid &>/dev/null; do
@@ -9,56 +9,66 @@ get_next_ctid() {
     echo $ctid
 }
 
-# Variables
+# Zmienne
 CTID=$(get_next_ctid)
 HOSTNAME="librenms"
 PASSWORD="librenms"
 TEMPLATE="debian-12-standard_12.0-1_amd64.tar.zst"
 BRIDGE="vmbr0"
 
-# Determine available storage
-STORAGE=$(pvesm status -content rootdir | awk 'NR==2{print $1}')
+# Wykrywanie dostępnego storage obsługującego szablony
+STORAGE=$(pvesm status -content vztmpl | awk 'NR==2{print $1}')
+if [ -z "$STORAGE" ]; then
+    echo "Brak dostępnego storage obsługującego szablony. Sprawdź konfigurację Proxmoxa."
+    exit 1
+fi
 
-# Download Debian 12 template if not already present
+# Pobieranie szablonu Debian 12, jeśli nie jest dostępny
 if ! pveam list $STORAGE | grep -q $TEMPLATE; then
     pveam update
     pveam download $STORAGE $TEMPLATE
 fi
 
-# Create and start the LXC container
+# Tworzenie i uruchamianie kontenera LXC
 pct create $CTID $STORAGE:vztmpl/$TEMPLATE --hostname $HOSTNAME --password $PASSWORD --net0 name=eth0,bridge=$BRIDGE,ip=dhcp --start 1
 
-# Wait for the container to obtain an IP address
+# Oczekiwanie na uzyskanie adresu IP przez kontener
 sleep 10
 
-# Get the IP address of the container
+# Pobieranie adresu IP kontenera
 IP=$(pct exec $CTID -- hostname -I | awk '{print $1}')
 
-# Define the URLs of the files to download
+# Sprawdzanie, czy kontener uzyskał adres IP
+if [ -z "$IP" ]; then
+    echo "Kontener nie uzyskał adresu IP. Sprawdź konfigurację sieci."
+    exit 1
+fi
+
+# Definiowanie URL-i plików do pobrania
 FILES=(
   "https://raw.githubusercontent.com/skippybossx/librenmp_lxc/main/ct/librenms.sh"
   "https://raw.githubusercontent.com/skippybossx/librenmp_lxc/main/install/librenms-install.sh"
   "https://raw.githubusercontent.com/skippybossx/librenmp_lxc/main/json/librenms.json"
 )
 
-# Define the target directories in the container
+# Definiowanie docelowych katalogów w kontenerze
 DIRECTORIES=(
   "/ct"
   "/install"
   "/json"
 )
 
-# Create directories and download files
+# Tworzenie katalogów i pobieranie plików
 for i in "${!FILES[@]}"; do
   pct exec $CTID -- mkdir -p "${DIRECTORIES[$i]}"
   pct exec $CTID -- wget -O "${DIRECTORIES[$i]}/$(basename ${FILES[$i]})" "${FILES[$i]}"
 done
 
-# Make the install script executable
+# Nadawanie uprawnień wykonywania dla skryptu instalacyjnego
 pct exec $CTID -- chmod +x /install/librenms-install.sh
 
-# Run the install script
+# Uruchamianie skryptu instalacyjnego
 pct exec $CTID -- /install/librenms-install.sh
 
-# Output the result
-echo "LibreNMS has been successfully installed and is available at http://$IP"
+# Wyświetlanie wyniku
+echo "LibreNMS został pomyślnie zainstalowany i jest dostępny pod adresem http://$IP"
